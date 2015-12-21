a1b9222f43457ca744440f7578403f096e7026b7
Thomas Boerger <tboerger@suse.de>
2015-12-21 18:05:12 +0100

    Initial real implementation

--- types.go
+++ types.go
@@ -1,7 +1,18 @@
 package main
 
 type Params struct {
-	AccessKeyID     string `json:"access_key_id"`
-	SecretAccessKey string `json:"secret_access_key"`
-	Region          string `json:"region"`
+	AccessKey          string `json:"access_key"`
+	SecretKey          string `json:"secret_key"`
+	Region             string `json:"region"`
+	Application        string `json:"application"`
+	DeploymentGroup    string `json:"deployment_group"`
+	DeploymentConfig   string `json:"deployment_config"`
+	Description        string `json:"description"`
+	RevisionType       string `json:"revision_type"`
+	BundleType         string `json:"bundle_type"`
+	BucketName         string `json:"bucket_name"`
+	BucketKey          string `json:"bucket_key"`
+	BucketEtag         string `json:"bucket_etag"`
+	BucketVersion      string `json:"bucket_version"`
+	IgnoreStopFailures bool   `json:"ignore_stop_failures"`
 }
